b776b7129406f3071f7403393bf3ff5956e2ba77
AnhKiet112k1 <kiet.nguyen112k1@hcmut.edu.vn>
2023-10-08 14:45:15 +0700

    Add keys (temporary)

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    googleClientID: '513408933598-f5ah9mpluoag58d00n0j2obct1ifjr66.apps.googleusercontent.com',
+    googleClientSecret: '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'
+};
